a4dc1cb2a91583e463770eed746f1cfe227b4f7c
HB-Krutagn <44233824+HB-Krutagn@users.noreply.github.com>
2019-09-16 17:40:03 +0530

    Update CalendarDateRangePickerViewController.swift

--- CalendarDateRangePickerViewController/Classes/CalendarDateRangePickerViewController.swift
+++ CalendarDateRangePickerViewController/Classes/CalendarDateRangePickerViewController.swift
@@ -122,11 +122,7 @@ extension CalendarDateRangePickerViewController {
             let date = getDate(dayOfMonth: dayOfMonth, section: indexPath.section)
             cell.date = date
             cell.label.text = "\(dayOfMonth)"
-            
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "yyyy-MM-dd"
-            let datePreFormatted = dateFormatter.string(from: date)
-            
+                        
             if disabledDates != nil{
                 if (disabledDates?.contains(cell.date!))!{
                     cell.disable()
