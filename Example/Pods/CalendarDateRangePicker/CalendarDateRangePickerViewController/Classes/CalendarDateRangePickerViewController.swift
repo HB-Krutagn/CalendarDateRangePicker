7b615f14576829cc73d9b079a91cdaec74b85e73
hb <hb@hbs-iMac-5.local>
2019-09-27 12:21:34 +0530

    Swift 5 Support

--- Example/Pods/CalendarDateRangePicker/CalendarDateRangePickerViewController/Classes/CalendarDateRangePickerViewController.swift
+++ Example/Pods/CalendarDateRangePicker/CalendarDateRangePickerViewController/Classes/CalendarDateRangePickerViewController.swift
@@ -122,7 +122,12 @@ extension CalendarDateRangePickerViewController {
             let date = getDate(dayOfMonth: dayOfMonth, section: indexPath.section)
             cell.date = date
             cell.label.text = "\(dayOfMonth)"
-                        
+            
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            let datePreFormatted = dateFormatter.string(from: date)
+            let dateFormatted = dateFormatter.date(from: datePreFormatted)
+            
             if disabledDates != nil{
                 if (disabledDates?.contains(cell.date!))!{
                     cell.disable()
